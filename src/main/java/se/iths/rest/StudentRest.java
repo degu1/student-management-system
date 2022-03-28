691be48acdf389f334018b406493615000e9238e
Dennis Gustafsson <dennisgust@gmail.com>
2022-03-28 16:19:59 +0200

    CRUD

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,56 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    StudentService service;
 
+    @POST()
+    public Response createStudent(Student student) {
+        service.createStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @PUT
+    public Response updateStudent(Student student) {
+        service.updateStudent(student);
+        return Response.ok().build();
+    }
+
+    @PATCH
+    public Response patchStudent(Student student) {
+        service.patchStudent(student);
+        return Response.ok().build();
+    }
+
+    @GET()
+    public Response getAllStudents() {
+        List<Student> students = service.getAllStudents();
+        return Response.ok(students).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getStudentById(@PathParam("id") Long id) {
+        return Response.ok(service.getStudentById(id)).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response deleteStudent(@PathParam("id") Long id) {
+        service.deleteStudent(id);
+        return Response.ok().build();
+    }
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,56 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
 public class StudentService {
+    @PersistenceContext
+    EntityManager entityManager;
 
+    public void createStudent(Student student) {
+        entityManager.persist(student);
+    }
 
+    public void updateStudent(Student student) {
+        entityManager.merge(student);
+    }
+
+    public void patchStudent(Student studentUpdate) {
+        Student student = entityManager.find(Student.class, studentUpdate.getId());
+        if (notNull(studentUpdate.getFirstName())) {
+            student.setFirstName(studentUpdate.getFirstName());
+        }
+        if (notNull(studentUpdate.getLastName())) {
+            student.setLastName(studentUpdate.getLastName());
+        }
+        if (notNull(studentUpdate.getEmail())) {
+            student.setEmail(studentUpdate.getEmail());
+        }
+        if (notNull(studentUpdate.getPhoneNumber())) {
+            student.setPhoneNumber(studentUpdate.getPhoneNumber());
+        }
+        entityManager.merge(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return entityManager.createQuery("SELECT s from Student s", Student.class).getResultList();
+    }
+
+    public Student getStudentById(Long id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    public void deleteStudent(Long id) {
+        Student student = entityManager.find(Student.class, id);
+        entityManager.remove(student);
+    }
+
+    private <T> boolean notNull(T object) {
+        return object != null;
+    }
 }
